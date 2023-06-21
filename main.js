2a131f3bff77794938d4d7f4071a1f9589673d55
Mohammad Ezlegini <131920344+Mammadez@users.noreply.github.com>
2023-06-21 18:32:11 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunc() {
+  var x = document.getElementById("mynav");
+  if (x.className === "navbar") {
+    x.className += " Mobile-responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+encodeURIComponent(www.mammadez.encodeURIComponent)
